3985a48e131338b13bd0d3634eaa5794eb462562
jocn <54664034+xtxujun@users.noreply.github.com>
2026-05-14 15:19:06 +0800

    Update TheDoc.vue

--- src/components/TheDoc.vue
+++ src/components/TheDoc.vue
@@ -1,14 +1,22 @@
 <script setup>
-
 </script>
 
 <template>
-  <!-- 可以在此添加一些样式 -->
   <div class="doc">
     <slot />
   </div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss"> /* 注意这里去掉了 scoped */
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  background-color: transparent !important;
+}
 
+.doc {
+  min-height: 100vh;
+  background-color: transparent;
+}
 </style>
