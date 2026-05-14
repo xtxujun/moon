afdc8c065f8931e814932bf92b0f9eaec618e4b4
jocn <54664034+xtxujun@users.noreply.github.com>
2026-05-14 14:10:47 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,15 @@
-<script setup>
-import { RouterView } from 'vue-router'
-import { ref, onMounted } from 'vue'
-
-const bingBg = ref('')
-const bingTitle = ref('')
-const bingCopyright = ref('')
-
-const fetchBingWallpaper = async () => {
-  try {
-    const res = await fetch('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN')
-    const data = await res.json()
-    const img = data.images[0]
-    bingBg.value = `https://www.bing.com${img.urlBase}_UHD.jpg`
-    bingTitle.value = img.title || 'Bing 每日美图'
-    bingCopyright.value = img.copyright || ''
-  } catch (e) {
-    bingBg.value = 'https://picsum.photos/1920/1080?random=1'
-  }
-}
-
-onMounted(() => {
-  fetchBingWallpaper()
-  setInterval(fetchBingWallpaper, 86400000) // 每天更新
-})
+<script setup lang="ts">
+// 1. 引入组件
+import BingBackground from './components/BingBackground.vue'
+// ... 其他原有的引入
 </script>
 
 <template>
-  <!-- Bing 背景 -->
-  <div class="fixed inset-0 bg-cover bg-center -z-10 transition-all duration-1000" 
-       :style="{ backgroundImage: `url(${bingBg})` }"></div>
-  
-  <!-- 毛玻璃叠加 -->
-  <div class="fixed inset-0 bg-black/40 backdrop-blur-[3px] -z-9"></div>
+  <!-- 2. 放置背景组件 (放在最前面) -->
+  <BingBackground />
 
-  <!-- 图片信息 -->
-  <div v-if="bingTitle" class="fixed bottom-6 right-6 text-white/90 text-right z-10 max-w-xs pointer-events-none">
-    <div class="text-sm">{{ bingTitle }}</div>
-    <div class="text-xs opacity-75">{{ bingCopyright }}</div>
+  <!-- 原有的页面内容 -->
+  <div class="app-container">
+     <!-- ... 原有代码 ... -->
   </div>
-
-  <RouterView />
 </template>
