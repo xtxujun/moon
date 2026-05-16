24a800d46fce30849340a4c63b0b168b52211944
jocn <54664034+xtxujun@users.noreply.github.com>
2026-05-16 17:38:11 +0800

    Update TheDoc.vue

--- src/components/TheDoc.vue
+++ src/components/TheDoc.vue
@@ -1,17 +1,7 @@
-<script setup>
-// 逻辑留空
-</script>
-
+<script setup></script>
 <template>
   <div class="doc">
     <slot />
   </div>
 </template>
-
-<style scoped>
-.doc {
-  min-height: 100vh;
-  /* 确保这里没有任何背景色，完全透明 */
-  background-color: transparent;
-}
-</style>
+<style scoped></style>
