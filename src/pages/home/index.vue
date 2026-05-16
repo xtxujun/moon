86b4a8995728bf1f4f12eabcb50fbc0963872c14
jocn <54664034+xtxujun@users.noreply.github.com>
2026-05-16 17:09:07 +0800

    Update index.vue

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -14,23 +14,15 @@ const settingStore = useSettingStore()
 
 <template>
   <TheDoc>
-    <div p="12 sm:24" w="full sm:auto" :class="{ 'no-select': settingStore.isSetting }">
+    <!-- 强制白色底 + 高层级 -->
+    <div class="relative z-30 min-h-screen p-12 sm:p-24 bg-white/80 dark:bg-black/80">
       <MainHeader />
 
       <MainClock v-if="!settingStore.isSetting" />
 
-      <!-- Bing 磨砂白透效果核心修改区域 -->
-      <div class="mt-6 max-w-[1380px] mx-auto relative z-10
-                  bg-white/70 dark:bg-black/60 
-                  backdrop-blur-2xl 
-                  rounded-3xl border border-white/30 dark:border-white/10 
-                  shadow-2xl overflow-hidden">
-        
-        <div class="p-8 sm:p-10">
-          <MainSearch v-if="!settingStore.isSetting" class="my-8" />
-          <SiteContainer :key="settingStore.siteContainerKey" />
-        </div>
-      </div>
+      <MainSearch v-if="!settingStore.isSetting" class="my-24" />
+
+      <SiteContainer :key="settingStore.siteContainerKey" />
 
       <MainSetting />
 
