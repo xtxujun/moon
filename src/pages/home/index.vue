3fd8f747f21e88cb7ce81c30cc74006570a8665c
jocn <54664034+xtxujun@users.noreply.github.com>
2026-05-16 18:40:14 +0800

    Update index.vue

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,15 +1,9 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import { useSettingStore } from '@/stores/setting' // 确保路径正确
-
-// 【关键修改】请根据你 src/components/ 下实际的文件名修改下面的路径
-// 如果文件名是 Header.vue，就写 './Header.vue' 或者 '@/components/Header.vue'
-import MainHeader from '@/components/Header.vue'
-import MainClock from '@/components/Clock.vue'
-import MainSearch from '@/components/Search.vue'
-import SiteContainer from '@/components/SiteContainer.vue'
-import MainSetting from '@/components/Setting.vue'
-import TheFooter from '@/components/TheFooter.vue' // 确认是否有这个文件，没有则删除引用
+import MainHeader from './components/MainHeader.vue'
+import MainClock from './components/MainClock.vue'
+import MainSearch from './components/MainSearch.vue'
+import SiteContainer from './components/SiteContainer.vue'
+import MainSetting from './components/MainSetting.vue'
 
 defineOptions({
   name: 'HomePage',
@@ -20,23 +14,22 @@ const settingStore = useSettingStore()
 
 <template>
   <TheDoc>
-    <!-- 这里的类名保持不变，确保背景透明 -->
-    <div class="relative z-[10000] min-h-screen p-12 sm:p-24 bg-transparent">
+    <div p="12 sm:24" bg="$main-bg-c" w="full sm:auto" :class="{ no_select: settingStore.isSetting }">
       <MainHeader />
-
       <MainClock v-if="!settingStore.isSetting" />
-
-      <MainSearch v-if="!settingStore.isSetting" class="my-24" />
-
+      <MainSearch v-if="!settingStore.isSetting" my-24 />
       <SiteContainer :key="settingStore.siteContainerKey" />
-
-      <MainSetting v-if="settingStore.isSetting" />
-
-      <TheFooter />
+      <MainSetting />
+      <TheFooter v-if="settingStore.getSettingValue('showFooter')" />
     </div>
+    <Blank />
   </TheDoc>
 </template>
 
-<style scoped>
-/* 确保没有局部样式覆盖背景 */
-</style>
+<route lang="yaml">
+path: /
+children:
+  - name: setting
+    path: setting
+    component: /src/components/Blank.vue
+</route>
