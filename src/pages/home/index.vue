241afc9a40457809c7ace52bf205c6baad400a9c
jocn <54664034+xtxujun@users.noreply.github.com>
2026-05-16 18:24:30 +0800

    Update index.vue

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import MainHeader from './components/MainHeader.vue'
-import MainClock from './components/MainClock.vue'
-import MainSearch from './components/MainSearch.vue'
-import SiteContainer from './components/SiteContainer.vue'
-import MainSetting from './components/MainSetting.vue'
+import MainHeader from '@/components/MainHeader.vue'
+import MainClock from '@/components/MainClock.vue'
+import MainSearch from '@/components/MainSearch.vue'
+import SiteContainer from '@/components/SiteContainer.vue'
+import MainSetting from '@/components/MainSetting.vue'
+import TheFooter from '@/components/TheFooter.vue' // 确保路径正确
 
 defineOptions({
   name: 'HomePage',
@@ -14,7 +15,8 @@ const settingStore = useSettingStore()
 
 <template>
   <TheDoc>
-    <div class="relative z-[10000] min-h-screen p-12 sm:p-24 bg-white/90 dark:bg-zinc-900/90">
+    <!-- 修改了这一行：删除了 bg-white/90 和 dark:bg-zinc-900/90 -->
+    <div class="relative z-[10000] min-h-screen p-12 sm:p-24 bg-transparent">
       <MainHeader />
 
       <MainClock v-if="!settingStore.isSetting" />
@@ -30,6 +32,7 @@ const settingStore = useSettingStore()
   </TheDoc>
 </template>
 
+<!-- 保留这段路由配置，它负责生成 /setting 页面 -->
 <route lang="yaml">
 path: /
 children:
