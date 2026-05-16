855ced7baf3b64498d66084589427b163488d1da
jocn <54664034+xtxujun@users.noreply.github.com>
2026-05-16 17:34:32 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import { ref, onMounted } from 'vue'
-
-const bingBg = ref('')
-const bingTitle = ref('')
-const bingCopyright = ref('')
-
-const fetchBingWallpaper = async () => {
-  try {
-    const res = await fetch('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN')
-    const data = await res.json()
-    const img = data.images[0]
-    bingBg.value = `https://www.bing.com${img.urlBase}_UHD.jpg`
-    bingTitle.value = img.title || 'Bing 每日美图'
-    bingCopyright.value = img.copyright || ''
-  } catch (e) {
-    bingBg.value = 'https://picsum.photos/1920/1080?random=1'
-  }
-}
-
-onMounted(() => {
-  fetchBingWallpaper()
-  setInterval(fetchBingWallpaper, 86400000)
-})
 </script>
 
 <template>
-  <div 
-    class="fixed inset-0 w-full h-full bg-cover bg-center z-[-1]" 
-    :style="{ backgroundImage: 'url(' + bingBg + ')' }"
-  ></div>
-  
-   <!-- 这里是你的页面内容插槽 -->
-  <RouterView />
+  <AppProvider>
+    <AppContainer>
+      <RouterView />
+    </AppContainer>
+  </AppProvider>
 </template>
