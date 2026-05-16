310d65a7e42961a21ac225f2d06a7f0714a37ca2
jocn <54664034+xtxujun@users.noreply.github.com>
2026-05-16 17:46:49 +0800

    Update TheDoc.vue

--- src/components/TheDoc.vue
+++ src/components/TheDoc.vue
@@ -1,7 +1,14 @@
-<script setup></script>
+<script setup>
+// 这里暂时不需要逻辑
+</script>
+
 <template>
-  <div class="doc">
+  <div class="min-h-screen flex flex-col">
+    <!-- 插槽：承载 index.vue 的内容 -->
     <slot />
   </div>
 </template>
-<style scoped></style>
+
+<style scoped>
+/* 确保没有任何背景色干扰 */
+</style>
