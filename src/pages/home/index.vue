074bccc9d9554e6eff93f01c6e66ad5af4e3305d
jocn <54664034+xtxujun@users.noreply.github.com>
2026-05-16 09:15:22 +0800

    Update index.vue

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -14,19 +14,22 @@ const settingStore = useSettingStore()
 
 <template>
   <TheDoc>
-    <div class="relative z-50 min-h-screen bg-white/95 dark:bg-zinc-950 p-8">
+    <div p="12 sm:24" bg="$main-bg-c" w="full sm:auto" :class="{ no_select: settingStore.isSetting }">
       <MainHeader />
-
       <MainClock v-if="!settingStore.isSetting" />
-
-      <div class="max-w-[1400px] mx-auto mt-8 bg-white dark:bg-zinc-900 rounded-3xl shadow-xl p-8">
-        <MainSearch v-if="!settingStore.isSetting" class="mb-10" />
-        <SiteContainer :key="settingStore.siteContainerKey" />
-      </div>
-
+      <MainSearch v-if="!settingStore.isSetting" my-24 />
+      <SiteContainer :key="settingStore.siteContainerKey" />
       <MainSetting />
-
       <TheFooter v-if="settingStore.getSettingValue('showFooter')" />
     </div>
+    <Blank />
   </TheDoc>
 </template>
+
+<route lang="yaml">
+path: /
+children:
+  - name: setting
+    path: setting
+    component: /src/components/Blank.vue
+</route>
